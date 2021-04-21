13d7bfbc9edd32b496520dc13df30704e0e4be8e
danielrosenman <44146635+danielrosenman@users.noreply.github.com>
2021-04-21 02:27:58 -0400

    Implemented viewVaccineSupply() in Government user

--- src/GovOfficial.java
+++ src/GovOfficial.java
@@ -9,7 +9,21 @@ public class GovOfficial extends AUser{
   GovOfficial(String userName, Connection connect){
     super(userName, connect);
   }
-public void viewVaccineSupply(){
+
+
+public void viewVaccineSupply() throws SQLException {
+  CallableStatement addStmt = connect.prepareCall("{CALL view_vaccine_supply ()}");
+  ResultSet result = addStmt.executeQuery();
+
+  while (result.next()) {
+    System.out.println("Vial Number: " + result.getInt(1) +
+        ", " + "Manufacturer Name: " + result.getString(2) +
+        "Interval Between Doses In Days :" + ", "
+        + result.getInt(3) + ", "
+        + "Number of Injections: " + result.getInt(4));
+  }
+
+
 
 }
 
@@ -18,7 +32,6 @@ public void viewVaccineSupply(){
     CallableStatement addStmt = connect.prepareCall("{CALL view_citizen_doses( ?)}");
     addStmt.setString(1, ssn);
     ResultSet result = addStmt.executeQuery();
-    System.out.println("Vaccine stock information successfully updated");
 
     while (result.next()) {
       System.out.println("Social Security Number: " + result.getString(1) +
